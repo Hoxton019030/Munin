0de89bf5b0124072ade8b539741a90ada4280a91
THINK-YICELWEN\yiwen <za546321@yahoo.com.tw>
2022-08-27 19:42:13 +0800

    feat : FilterConfig done

---
+++ src/main/java/com/raven/munin/config/FilterConfig.java
@@ -0,0 +1,21 @@
+package com.raven.munin.config;
+
+import com.raven.munin.filter.LogProcessTimeFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean logProcessTimeFilter() {
+        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new LogProcessTimeFilter()); //設定想要使用哪一個Filter
+        bean.addUrlPatterns("/*"); //設置哪些url會觸發Filter，設置成/* 就代表全部都會吃到，/user/*就代表/user開頭的都會吃到
+        bean.setName("logProcessTimeFilter"); //設置要叫什麼名字
+        return bean;
+    }
+}
