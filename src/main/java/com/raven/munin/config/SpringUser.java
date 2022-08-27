fc3ea07009cba99b972eff08f874993155be2042
THINK-YICELWEN\yiwen <za546321@yahoo.com.tw>
2022-08-27 18:54:43 +0800

    feat : rename Member-code to Member-password

--- src/main/java/com/raven/munin/config/SpringUser.java
+++ src/main/java/com/raven/munin/config/SpringUser.java
@@ -1,24 +1,34 @@
 package com.raven.munin.config;
 
+import com.raven.munin.enumeration.MemberAuthority;
+import com.raven.munin.model.entity.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 /**
  * 一個練習，練習如何自定義複雜一點的身分驗證
  */
 public class SpringUser implements UserDetails {
 
+    @Autowired
+    Member member;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        ArrayList<MemberAuthority> authority = new ArrayList<>();
+        authority.add(member.getAuthority());
+        return authority.stream().map(auth -> new SimpleGrantedAuthority(auth.name())).collect(Collectors.toList());
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return member.getCode();
     }
 
     @Override
