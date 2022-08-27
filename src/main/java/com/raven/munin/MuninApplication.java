381ae209bc0bc6e73fa238fc31f447239306d7c2
THINK-YICELWEN\yiwen <za546321@yahoo.com.tw>
2022-08-27 09:58:23 +0800

    re-start

---
+++ src/main/java/com/raven/munin/MuninApplication.java
@@ -0,0 +1,13 @@
+package com.raven.munin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuninApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuninApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raven/munin/MuninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raven.munin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuninApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
