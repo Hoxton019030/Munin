8bae95c8f47a038341128bc88412a8d24895b741
THINK-YICELWEN\yiwen <za546321@yahoo.com.tw>
2022-08-28 04:32:15 +0800

    feat : adjust sercurity config

--- src/main/java/com/raven/munin/config/FilterConfig.java
+++ src/main/java/com/raven/munin/config/FilterConfig.java
@@ -17,7 +17,7 @@ public class FilterConfig {
         bean.setFilter(new LogProcessTimeFilter()); //設定想要使用哪一個Filter
         bean.addUrlPatterns("/*"); //設置哪些url會觸發Filter，設置成/* 就代表全部都會吃到，/user/*就代表/user開頭的都會吃到
         bean.setName("logProcessTimeFilter"); //設置要叫什麼名字
-//        bean.setOrder(0); //設定過濾器的執行順序
+        bean.setOrder(0); //設定過濾器的執行順序
         return bean;
     }
 
