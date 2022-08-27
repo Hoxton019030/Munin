ca505ba08e907fb040e660e1d8d32ff1a66b4f33
THINK-YICELWEN\yiwen <za546321@yahoo.com.tw>
2022-08-27 12:12:08 +0800

    feat : create member entity

--- src/main/java/com/raven/munin/config/SecurityConfig.java
+++ src/main/java/com/raven/munin/config/SecurityConfig.java
@@ -1,21 +1,21 @@
-package com.raven.munin.config;
-
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .antMatchers(HttpMethod.GET, "/users/**").authenticated()
-                .antMatchers(HttpMethod.GET).permitAll()
-                .antMatchers(HttpMethod.POST, "/users").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable()
-                .formLogin();
-    }
-}
+//package com.raven.munin.config;
+//
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    protected void configure(HttpSecurity http) throws Exception {
+////        http.authorizeHttpRequests()
+////                .antMatchers(HttpMethod.GET, "/users/**").authenticated()
+////                .antMatchers(HttpMethod.GET).permitAll()
+////                .antMatchers(HttpMethod.POST, "/users").permitAll()
+////                .anyRequest().permitAll()
+////                .and()
+////                .csrf().disable()
+////                .formLogin();
+//    }
+//}
---
+++ src/main/java/com/raven/munin/model/entity/Member.java
@@ -0,0 +1,52 @@
+
+package com.raven.munin.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SEQ", columnDefinition = "int", nullable = false)
+    private Integer seq;
+
+    /**
+     * Encrypto: identity Card number
+     */
+    @Column(name = "ID", columnDefinition = "CHARACTER VARYING(150)", nullable = false, length = 150)
+    private String id;
+
+    /**
+     * virtual avatar name
+     */
+    @Column(name = "NAME", columnDefinition = "CHARACTER VARYING(200)", nullable = false)
+    private String name;
+
+    /**
+     * password(hashcode)
+     */
+    @Column(name = "CODE", columnDefinition = "CHARACTER VARYING(500)", nullable = false)
+    private String code;
+
+    /**
+     * create time
+     */
+    @Column(name = "CREATE_TIME", columnDefinition = "TIMESTAMP default FORMATDATETIME(LOCALTIMESTAMP(), 'yyyy-MM-dd HH:mm:ss')", nullable = false)
+    private LocalDateTime createTime;
+
+
+    /**
+     * update time
+     */
+    @Column(name = "UPDATE_TIME", columnDefinition = "TIMESTAMP", nullable = true)
+    private LocalDateTime updateTime;
+
+
+}
+
+
+
