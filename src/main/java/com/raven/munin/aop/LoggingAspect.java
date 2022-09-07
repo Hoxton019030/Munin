74474ac371b504e8576cca6a876e8d9a3c656034
Hoxton Hsu <hoxton.hsu@eunodata.com>
2022-09-07 14:49:34 +0800

    123

---
+++ src/main/java/com/raven/munin/aop/LoggingAspect.java
@@ -0,0 +1,43 @@
+package com.raven.munin.aop;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * @author Hoxton
+ * @version 1.1.0
+ */
+@Aspect
+@Configuration
+public class LoggingAspect {
+    private static final Logger logger = LogManager.getLogger(LoggingAspect.class);
+
+    /**
+     * @param joinPoint\
+     * @return Object代表目標的回傳值
+     */
+    @Around("@within(org.springframework.web.bind.annotation.RestController)) && within(com." )
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.debug("Request for {}.{}() with arguments [s] = {}" ,joinPoint.getSignature().getDeclaringTypeName(),joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        Instant start = Instant.now();
+        Object returnValue = joinPoint.proceed();
+        Instant finish = Instant.now();
+
+        long timeElapsed = Duration.between(start, finish).toMillis();
+
+        logger.debug("Response for {}.{} with Result ={}",joinPoint.getSignature().getDeclaringTypeName(),joinPoint.getSignature().getName(),returnValue);
+        logger.info("Time Taken =" + new SimpleDateFormat("mm:ss:SSS").format(new Date(timeElapsed)));
+        return returnValue;
+    }
+
+}
