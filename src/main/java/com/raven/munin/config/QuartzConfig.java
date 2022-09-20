1f05d5183533b154ae7091a23f6d8613e451de1b
Hoxton Hsu <hoxton.hsu@eunodata.com>
2022-09-20 13:55:22 +0800

    delete webconfig(not usage)

---
+++ src/main/java/com/raven/munin/config/QuartzConfig.java
@@ -0,0 +1,12 @@
+package com.raven.munin.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Hoxton
+ * @version 1.1.0
+ */
+@Configuration
+public class QuartzConfig {
+
+}
